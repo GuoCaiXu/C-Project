6b1e2bbae2ceaeee5dcff05bedea5b2ade613bf4
徐国彩 <xgc030920@outlook.com>
2022-10-20 18:23:50 +0800

    更新

--- 蓝桥杯题目/实验.cpp
+++ 蓝桥杯题目/实验.cpp
@@ -1,10 +1,20 @@
-# include <stdio.h>
-# include <math.h>
+# include <iostream>
+# define e 2.718281828459
+using namespace std;
+
 
 int main() {
 
-	double p = pow(1 + 0.1, 10);
-	printf("%.3f", p);
+	int x;
+	double result;
+	cin >> x;
+	if (x <= 1) {
+		result = pow(e, x);
+	}
+	else if (x > 1) {
+		result = pow(x, 2) - 1;
+	}
+	printf("f(%.1lf) = %.1lf", x, result);
 
 	return 0;
 }
