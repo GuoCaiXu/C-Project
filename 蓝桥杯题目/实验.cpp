2a317b069330416d96d3befc3ae835c20d03dab4
徐国彩 <xgc030920@outlook.com>
2022-10-23 16:08:52 +0800

    更新

--- 蓝桥杯题目/实验.cpp
+++ 蓝桥杯题目/实验.cpp
@@ -1,24 +1,21 @@
 # include <iostream>
+# include <algorithm>
+
 using namespace std;
 
 int main() {
 
-    char c;
-    cin >> c;
-    double a[12][12], num = 0;
-    for (int i = 0; i < 12; i++) {
-        for (int j = 0; j < 12; j++) {
-            cin >> a[i][j];
-        }
-    }
-    for (int i = 11; i < 6; i--) {
-        for (int j = 12 - i; j < i; j++) {
-            num += a[j][i];
-        }
-    }
-    if (c == 'S') printf("%.1lf", num);
+    int x, y;
+    while (cin >> x >> y, x > 0 && y > 0) {
+        if (x > y) swap(x, y);
 
-    else if (c == 'M') printf("%.1lf", num / 30);
+        int sum = 0;
+        for (int i = x; x <= y; i++) {
+            cout << i << " ";
+            sum += i;
+        }
+        cout << "Sum=" << sum << endl;
+    }
 
     return 0;
 }
