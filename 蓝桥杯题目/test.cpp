bcd53f59673931e56b643279780524e22a9181c8
徐国彩 <xgc030920@outlook.com>
2022-11-09 21:09:34 +0800

    更新

--- 蓝桥杯题目/test.cpp
+++ 蓝桥杯题目/test.cpp
@@ -1,12 +1,17 @@
-# include <iostream>
-
-using namespace std;
+# include <stdio.h>
 
 int main() {
 
-	int a = +30;
+	int day[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	int mouth;
 
-	cout << a << endl;
+	puts("请输入月份:");
+	scanf_s("%d", &mouth);
+
+	if (mouth - 1 >= 0 && mouth - 1 <= 11) {
+		printf("%d月有%d天", mouth, day[mouth - 1]);
+	}
+	else puts("输入有误！");
 
 	return 0;
 }
